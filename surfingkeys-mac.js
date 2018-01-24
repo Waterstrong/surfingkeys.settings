2171c55adf0ef8cf11294e4385cebb46cc12e442
Waterstrong <sqlin@thoughtworks.com>
2018-01-24 11:11:36 +0800

    update the surfingkeys key map for mac

--- surfingkeys-mac.js
+++ surfingkeys-mac.js
@@ -8,18 +8,22 @@ unmap('<Ctrl-p');
 // remove a keystroke mapping for Insert mode
 iunmap('<Ctrl-f>');
 iunmap('<Ctrl-e>');
-iunmap('<Ctrl-n>');
-iunmap('<Ctrl-p>');
+// iunmap('<Ctrl-n>');
+// iunmap('<Ctrl-p>');
 iunmap('<Alt-b>');
 iunmap('<Alt-f>');
 iunmap('<Alt-w>');
 iunmap('<Alt-d>');
 
+// add a keystroke mapping
+map('<Ctrl-c>', '<Esc>');
+map('u', 'e');
+
 // add a keystroke mapping for Insert mode
-imap('<Ctrl-c>', "<Esc>");
+// imap('<Ctrl-c>', '<Esc>');
 
 // add a keystroke mapping for Visual mode
-vmap('<Ctrl-c>', "<Esc>")
+// vmap('<Ctrl-c>', '<Esc>')
 
 // add a keystroke mapping for Omnibar mode
-cmap('<Ctrl-c>', "<Esc>")
+// cmap('<Ctrl-c>', '<Esc>')
